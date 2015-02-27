8b7bdd7b3d8a8a7477f2a67213423f3fe736976d
ciscou <francismpp@gmail.com>
2015-02-27 14:31:38 +0100

    included backbone marionette

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,7 @@
 //= require vendor/_jquery-2.1.1
 //= require vendor/_underscore
 //= require vendor/_backbone
+//= require vendor/_backbone.marionette
 //= require vendor/_backbone.localStorage
 //= require vendor/_backbone.obscura
 //= require bootstrap
